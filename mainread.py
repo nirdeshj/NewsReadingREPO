e6d2a23f7c922685ae5fc9cbcdfb55fca362f5dd
NirdeshJ <nirdeshjoshi1@gmail.com>
2026-05-06 12:18:05 +0545

    minor changes to mainread

--- mainread.py
+++ mainread.py
@@ -5,7 +5,7 @@ import sys
 import os 
 
 load_dotenv()
-MAX_NO_OF_ARTICLES = 15
+MAX_NO_OF_ARTICLES = 20
 API_KEY = os.getenv("NEWS_API_KEY")
 
 if not API_KEY:
@@ -29,7 +29,7 @@ def processnews(r):
     result_news_dict = r.json()
     articles = result_news_dict.get('articles', [])
     
-    #print(f"\n{t.title()} (Total {result_news_dict['totalResults']}):")
+
 
     if not articles:
          return []
