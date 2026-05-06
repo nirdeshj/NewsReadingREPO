f3febd2720afe04c5a9974ca9c1df96aab3b87eb
NirdeshJ <nirdeshjoshi1@gmail.com>
2026-05-06 12:19:42 +0545

    only acceps comma separated inputs

--- userinput.py
+++ userinput.py
@@ -1,23 +1,14 @@
 
 
-def main():
-    userInput = input("Enter the topics you're interested in (just separate topics by space): ").lower()
+def main(userInput):
     spacesep = split_input(userInput)
     return spacesep 
     
 
 def split_input(userInput):
     spacesepvalues = None
-
-    if (userInput.find(',') == -1):
-        spacesepvalues = userInput.split(' ')
-        spacesepvalues = [x.strip() for x in spacesepvalues]
-        return spacesepvalues
-    else:
-        spacesepvalues = userInput.split(',')
-        spacesepvalues = [x.strip() for x in spacesepvalues]
-        return spacesepvalues
+    spacesepvalues = userInput.split(',')
+    spacesepvalues = [x.strip().lower() for x in spacesepvalues]
+    return spacesepvalues
 
 
-if __name__ == '__main__':
-    main()
